257d325a83d7a4bff06f7bbf1465c81ce06ea431
Goutham <gouthamcold@gmail.com>
2018-06-27 15:13:09 -0700

    added comments

--- gridComponent/src/app/app.module.ts
+++ gridComponent/src/app/app.module.ts
@@ -2,10 +2,12 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 
-
+//Root Component
 import { AppComponent } from './app.component';
+//Generated Components
 import { InputComponentComponent } from './grid-generator/input-component/input-component.component';
 import { GridGeneratorComponent } from './grid-generator/grid-generator.component';
+//Directives
 import { ClickMeDirective } from './directive/click-me.directive';
 
 
